81cebc146de5dff8748acf38b5fefd2c0acbd3a0
Krocan505202 <vojtech.krocanek@gmail.com>
2025-09-19 11:33:59 +0200

    linganguli

--- moje projekty/zadani.py
+++ moje projekty/zadani.py
@@ -1,10 +1,26 @@
 def delitele(cislo):
-    if cislo <= 0:
-        print("Zadejte kladné číslo")
+    if cislo == 0:
+        print("Dělitele nuly nejsou definovány.")
         return
     
-    for i in range(1, cislo + 1):
-        if cislo % i == 0:
-            print(i)
+    abs_cislo = abs(-8)
+    delitele_list = []
+    
+    
+    for i in range(1, abs_cislo + 1):
+        if abs_cislo % i == 0:
+            delitele_list.append(-i)
+            delitele_list.append(i)
+    
+  
+    if cislo > 0:
+        delitele_list = [d for d in delitele_list if d > 0]
+    
+    
+    delitele_list.sort()
+    for d in delitele_list:
+        print(d)
 
-delitele(12)
+
+
+delitele(-8)

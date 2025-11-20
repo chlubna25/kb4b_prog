5a34212583204010b87b3c9a1ea984f351384ce5
Jebus-bus <st025526@student.cichnovabrno.cz>
2025-11-20 10:17:33 +0000

    dih

--- 0. uvodni_opakovani/reseni_uloh/pan_ucitel_chlubna.py
+++ 0. uvodni_opakovani/reseni_uloh/pan_ucitel_chlubna.py
@@ -1,2 +1,13 @@
 import random
 
+min = int(input("Zadej minimální hodnotu intervalu: "))
+max = int(input("Zadej maximální hodnotu intervalu: "))
+amount = int(input("Zadej počet náhodných čísel k vygenerování: "))
+
+random_numbers = [random.randint(min, max) for _ in range(amount)]
+
+soubor = "2. prace_se_soubory/data/cisla.txt"
+
+with open(soubor, "w", encoding="utf-8") as file:
+    for number in random_numbers:
+        file.write(f"{number}\n")

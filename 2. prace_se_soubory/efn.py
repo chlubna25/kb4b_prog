6610dbfbc4b8737fb47c73b78963f28a9bf3f9ef
jebis <jonatan.vemeno@gmail.com>
2025-12-21 20:37:24 +0100

    eskibiti doiled

--- 2. prace_se_soubory/efn.py
+++ 2. prace_se_soubory/efn.py
@@ -1,4 +1,84 @@
-hra():
+import csv
+import random
+import matplotlib.pyplot as plt
+
+def hra():
+    otazky_easy = []
+    otazky_medium = []
+    otazky_hard = []
+
+    otazky = "2. prace_se_soubory\PROJEKT_milionar\quiz_questions.csv"
+
+    with open(otazky, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for radek in reader:
+            if radek["difficulty"] == "easy":
+                otazky_easy.append(radek)
+            elif radek["difficulty"] == "medium":
+                otazky_medium.append(radek)
+            elif radek["difficulty"] == "hard":
+                otazky_hard.append(radek)
+
+    vybrane_otazky = (
+        random.sample(otazky_easy, 5) +
+        random.sample(otazky_medium, 5) +
+        random.sample(otazky_hard, 5)
+    )
+
+    print("\n hra zacina ted \n")
+
+    cislo = 1
+    for otazka in vybrane_otazky:
+        print(f"otazecka {cislo}: {otazka['question']}")
+        odpoved = input("T / F: ").strip().upper()
+
+
+        if odpoved == "T":
+            odpoved_bool = "TRUE"
+        elif odpoved == "F":
+            odpoved_bool = "FALSE"
+        else:
+            print("zadej pouze T nebo F! pokud zadas neco jineho tak to pro tebe nedopadne dobre")
+            return
+
+
+        spravna = otazka["correct_answer"].strip().upper()
+
+        if odpoved_bool != spravna:
+            print("\n spatna odpoved. smula, hra je na konecku.")
+            return
+
+        print(" spravna odpoved\n")
+
+
+    print(" gratuluju. vyhral jsi zlateho bludistaka a dobry pocit k tomu")
+
+def graf():
+    easy = 0
+    medium = 0
+    hard = 0
+
+    otazecky = "2. prace_se_soubory\PROJEKT_milionar\quiz_questions.csv"
+    with open(otazecky, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for radek in reader:
+            if radek["difficulty"] == "easy":
+                easy += 1
+            elif radek["difficulty"] == "medium":
+                medium += 1
+            elif radek["difficulty"] == "hard":
+                hard += 1
+
+
+    labels = ["easy", "medium", "hard"]
+    hodnoty = [easy, medium, hard]
+
+
+    plt.figure()
+    plt.pie(hodnoty, labels=labels, autopct="%1.1f%%")
+    plt.title("podil obtiznosti otazecek v kvizecku")
+
+    plt.show()
 
 
 print("vitej cernochu v onoline nejlepism kasinu kde se stanes milionarem.")
@@ -18,7 +98,7 @@ if epstein == "P":
                     hra()
 
                 elif vyber == "G":
-                    pass
+                    graf()
 
                 else:
                     print("Asi vazne neumis psat nebo nevim cichna te spapa a nebuides stastnz.")
